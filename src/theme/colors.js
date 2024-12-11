8d90e8866a4bd4eda1de61b8d39deab3918b7e72
bublibub <shourya.batra@gmail.com>
2024-12-10 22:24:44 -0800

    add more colors, make all colors hex

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,6 +1,7 @@
 const colors = {
-  darkenedRed: "rgb(102, 33, 49)",
-  lightRed: "rgba(240, 233, 234, 1)",
+  darkenedRed: "#662131",
+  lightRed: "#efe8e9",
+  blackText: "#212121",
 };
 
 export default colors;
