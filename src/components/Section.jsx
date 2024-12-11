12b570efbfef057730637a28b5b3bd0b85fd4d9d
bublibub <shourya.batra@gmail.com>
2024-12-10 22:25:28 -0800

    feat: Section component which displays header and text

---
+++ src/components/Section.jsx
@@ -0,0 +1,35 @@
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import colors from "../theme/colors";
+
+function Section({ children, headingText }) {
+  return (
+    <Box>
+      <Box
+        sx={{
+          backgroundColor: colors.lightRed,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100px",
+          margin: "10px",
+        }}
+      >
+        <Typography
+          sx={{
+            fontFamily: "roboto",
+            color: colors.darkenedRed,
+            fontSize: "25px",
+            fontWeight: "900",
+          }}
+        >
+          {headingText}
+        </Typography>
+      </Box>
+
+      <Box>{children}</Box>
+    </Box>
+  );
+}
+
+export default Section;
