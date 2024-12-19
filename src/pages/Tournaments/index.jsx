a0475a2393db11e101ccab00d5bf5fd29e6584b6
Shourya Batra <51655190+ShouryaBatra@users.noreply.github.com>
2024-12-19 09:39:55 -0800

    meetings page

--- src/pages/Tournaments/index.jsx
+++ src/pages/Tournaments/index.jsx
@@ -1,7 +1,104 @@
+import { Box, Typography } from "@mui/material";
+import colors from "../../theme/colors";
+
 export default function Tournaments() {
   return (
     <>
-      <h1>Tournaments</h1>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: 20,
+              fontWeight: 700,
+              fontFamily: "Roboto Condensed",
+              padding: "10px",
+            }}
+          >
+            Sign up for upcoming tournaments here!
+          </Typography>
+
+          <Typography
+            sx={{
+              fontFamily: "Roboto Condensed",
+              color: colors.darkenedRed,
+              display: "flex",
+              flexDirection: "row",
+              padding: "10px",
+              fontSize: "19px",
+            }}
+          >
+            REMINDER: All steps on the{" "}
+            <i>
+              <u>
+                <a
+                  href="https://docs.google.com/document/d/1QYjrK-zeM_lS1vL-vqxIQTz4etOmpI9Wt-wqK6_srjk/edit?tab=t.0"
+                  style={{
+                    color: "black",
+                  }}
+                >
+                  {" "}
+                  Competitor Checklist{" "}
+                </a>
+              </u>
+            </i>
+            MUST be completed in order to compete
+          </Typography>
+        </Box>
+
+        <Box sx={{ paddingX: "100px", paddingBottom: "40px" }}>
+          <Typography
+            sx={{
+              fontFamily: "Roboto Condensed",
+              color: colors.darkenedRed,
+              display: "flex",
+              flexDirection: "row",
+              padding: "10px",
+              fontSize: "19px",
+            }}
+          >
+            Upcoming tournaments
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "Roboto Condensed",
+              display: "flex",
+              flexDirection: "row",
+              padding: "10px",
+              fontSize: "19px",
+            }}
+          >
+            <a href="https://forms.gle/kfwMfaJuUFLZUZMA7">CFL Policy/LD</a>
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "Roboto Condensed",
+              display: "flex",
+              flexDirection: "row",
+              padding: "10px",
+              fontSize: "19px",
+            }}
+          >
+            <a href="https://forms.gle/7EYV49E922N5ZwoN6">CFL Parli/PF</a>
+          </Typography>
+          <Typography
+            sx={{
+              fontFamily: "Roboto Condensed",
+              display: "flex",
+              flexDirection: "row",
+              padding: "10px",
+              fontSize: "19px",
+            }}
+          >
+            <a href="https://forms.gle/sGTikUthe3L4g1f57">John Lewis SVUDL</a>
+          </Typography>
+        </Box>
+      </Box>
     </>
   );
 }
