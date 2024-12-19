37516ff6ff19833e3c70fffd984f440fcaa3322c
Shourya Batra <51655190+ShouryaBatra@users.noreply.github.com>
2024-12-19 10:21:53 -0800

    start working on links page

--- src/pages/Links/index.jsx
+++ src/pages/Links/index.jsx
@@ -1,7 +1,19 @@
+import Section from "../../components/Section";
+import LinktreeBox from "../../components/LinktreeBox";
+import { Box } from "@mui/material";
+
 export default function Links() {
   return (
     <>
-      <h1>Links</h1>
+      <Section headingText={"Linktree"}>
+        <Box sx={{ padding: "50px" }}>
+          <LinktreeBox width={"450px"} />
+        </Box>
+      </Section>
+
+      <Section headingText={"For Students"}></Section>
+
+      <Section headingText={"For Parents"}></Section>
     </>
   );
 }
