ce3215c3a75bc0b13eabc0b71e6b4c6deb963638
Shourya Batra <51655190+ShouryaBatra@users.noreply.github.com>
2024-12-19 18:39:42 -0800

    first Section of links page, started second Section

--- src/pages/Links/index.jsx
+++ src/pages/Links/index.jsx
@@ -1,6 +1,6 @@
 import Section from "../../components/Section";
 import LinktreeBox from "../../components/LinktreeBox";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function Links() {
   return (
@@ -11,7 +11,40 @@ export default function Links() {
         </Box>
       </Section>
 
-      <Section headingText={"For Students"}></Section>
+      <Section headingText={"For Students"}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            paddingX: "150px",
+            paddingY: "50px",
+          }}
+        >
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <Typography sx={{ paddingY: "10px" }}>
+              Form to link your tabroom account:
+            </Typography>
+
+            <Box sx={{ paddingY: "10px" }}>
+              <Typography>Submit your monster packet:</Typography>
+              <ul>
+                <li>
+                  <Typography>Upload as a PDF to: </Typography>
+                </li>
+              </ul>
+            </Box>
+
+            <Typography sx={{ paddingY: "10px" }}>
+              NSDA Membership (after paying):
+            </Typography>
+          </Box>
+
+          <Box sx={{ display: "flex", flexDirection: "row" }}>
+            <Typography>MONSTER PACKETS:</Typography>
+            <Typography>Competitor Checklist: link</Typography>
+          </Box>
+        </Box>
+      </Section>
 
       <Section headingText={"For Parents"}></Section>
     </>
