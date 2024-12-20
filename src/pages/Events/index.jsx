b51a01d20160b171e397f8592632bd78ef432c1c
Shourya Batra <51655190+ShouryaBatra@users.noreply.github.com>
2024-12-20 10:26:14 -0800

    start on events page

--- src/pages/Events/index.jsx
+++ src/pages/Events/index.jsx
@@ -1,8 +1,28 @@
+import { Box, Typography } from "@mui/material";
+import Section from "../../components/Section";
+
 export default function Events() {
-    return (
-      <>
-        <h1>Events</h1>
-      </>
-    );
-  }
-  
+  return (
+    <>
+      <Box>
+        <Typography
+          sx={{
+            fontFamily: "Roboto Condensed",
+            display: "flex",
+            paddingX: "175px",
+            paddingY: "20px",
+            justifyContent: "center",
+            fontSize: "19px",
+            textAlign: "center",
+          }}
+        >
+          At SDC, we offer a variety of Speech and Debate events, each led by
+          1-2 committee leaders. Students can compete in these events at
+          tournaments throughout the year.
+        </Typography>
+
+        <Section headingText={"Speech Events"}></Section>
+      </Box>
+    </>
+  );
+}
