a76f53442a14fa41649025ec6a844356908a1495
bublibub <shourya.batra@gmail.com>
2024-12-21 13:13:40 -0800

    about page

---
+++ src/constants/aboutImages.js
@@ -0,0 +1,86 @@
+import kristinLee from "../assets/images/aboutPageImages/kristinLee.png";
+import kevinFei from "../assets/images/aboutPageImages/kevinFei.png";
+import peterWatts from "../assets/images/aboutPageImages/peterWatts.png";
+import henryHu from "../assets/images/aboutPageImages/henryHu.png";
+import evangelinePark from "../assets/images/aboutPageImages/evangelinePark.png";
+import lunaCheng from "../assets/images/aboutPageImages/lunaCheng.png";
+import saraMohsin from "../assets/images/aboutPageImages/saraMohsin.png";
+import chijulTripathy from "../assets/images/aboutPageImages/chijulTripathy.png";
+import annikaAbraham from "../assets/images/aboutPageImages/annikaAbraham.png";
+import rahimaNazirudeen from "../assets/images/aboutPageImages/raheemaNazirudeen.png";
+import poorvaKhambekar from "../assets/images/aboutPageImages/poorvaKhambekar.png";
+import advayRanade from "../assets/images/aboutPageImages/advayRanade.png";
+
+export const aboutImages = {
+  adminOfficers: {
+    kristinLee: {
+      name: "Kristin Lee",
+      position: "President",
+      img: kristinLee,
+    },
+    kevinFei: {
+      name: "Kevin Fei",
+      position: "Vice President",
+      img: kevinFei,
+    },
+    peterWatts: {
+      name: "Peter Watts",
+      position: "Vice President",
+      img: peterWatts,
+    },
+    henryHu: {
+      name: "Henry Hu",
+      position: "Secretary",
+      img: henryHu,
+    },
+    evangelinePark: {
+      name: "Evangeline Park",
+      position: "Social Manager/ Treasurer",
+      img: evangelinePark,
+    },
+  },
+  speechCommitteeLeaders: {
+    lunaCheng: {
+      name: "Luna Cheng",
+      position: "Interpretation",
+      img: lunaCheng,
+    },
+    saraMohsin: {
+      name: "Sara Mohsin",
+      position: "Original",
+      img: saraMohsin,
+    },
+    henryHu: {
+      name: "Henry Hu",
+      position: "Impromptu",
+      img: henryHu,
+    },
+  },
+  debateCommitteeLeaders: {
+    chijulTripathy: {
+      name: "chijulTripathy",
+      position: "Lincoln-Douglas",
+      img: chijulTripathy,
+    },
+    annikaAbraham: {
+      name: "Annika Abraham",
+      position: "Lincoln-Douglas",
+      img: annikaAbraham,
+    },
+    rahimaNazirudeen: {
+      name: "Rahima Nazirudeen",
+      position: "Public Forum",
+      img: rahimaNazirudeen,
+    },
+    poorvaKhambekar: {
+      name: "Poorva Khambekar",
+      position: "Public Forum",
+      img: poorvaKhambekar,
+    },
+    advayRanade: {
+      name: "Advay Ranade",
+      position: "Congress",
+      img: advayRanade,
+    },
+  },
+};
--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,26 @@
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  Grid,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  Typography,
+} from "@mui/material";
 import colors from "../../theme/colors";
+import { aboutImages } from "../../constants/aboutImages";
 
 export default function About() {
+  const adminOfficers = Object.values(aboutImages.adminOfficers);
+  const speechCommitteeLeaders = Object.values(
+    aboutImages.speechCommitteeLeaders
+  );
+  const debateCommitteeLeaders = Object.values(
+    aboutImages.debateCommitteeLeaders
+  );
+
   return (
     <>
       <Box sx={{ display: "flex", justifyItems: "center", padding: "150px" }}>
@@ -24,14 +43,208 @@ export default function About() {
         </Typography>
       </Box>
 
-      <Box
+      {/* <Box
         sx={{
           backgroundColor: colors.darkenedRed,
           display: "flex",
           alignContent: "center",
         }}
       >
-        <Typography>Meet the Team</Typography>
+        <Typography >Meet the Team</Typography>
+        <Box>
+          <ImageList sx={{ width: 500, height: 450 }}>
+            {adminOfficers.map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  src={`${item.img}?w=248&fit=crop&auto=format`}
+                  alt={item.name}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={item.name}
+                  subtitle={<span>{item.position}</span>}
+                  position="below"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </Box>
+      </Box> */}
+
+      <Box
+        sx={{
+          backgroundColor: colors.darkenedRed, // Darkened red
+          minHeight: "100vh",
+          padding: "2rem",
+        }}
+      >
+        {/* Title */}
+        <Typography
+          variant="h4"
+          component="h2"
+          align="center"
+          gutterBottom
+          sx={{
+            color: "white",
+            fontWeight: "bold",
+            marginBottom: "2rem",
+          }}
+        >
+          Meet the Team
+        </Typography>
+
+        {/* Admin Officers */}
+        <Box sx={{ textAlign: "center", marginBottom: "2rem" }}>
+          <Typography
+            variant="h6"
+            sx={{ color: "white", fontWeight: "bold", marginBottom: "1rem" }}
+          >
+            Admin Officers:
+          </Typography>
+
+          <Grid container spacing={4} justifyContent="center">
+            {adminOfficers.map((officer) => (
+              <Grid item key={officer.name} xs={12} sm={6} md={4} lg={2}>
+                <Card
+                  sx={{
+                    backgroundColor: "#fff",
+                    borderRadius: "8px",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    image={officer.img}
+                    alt={officer.name}
+                    sx={{
+                      height: "200px",
+                      objectFit: "cover",
+                    }}
+                  />
+                  <CardContent>
+                    <Typography
+                      variant="subtitle1"
+                      align="center"
+                      sx={{ fontWeight: "bold" }}
+                    >
+                      {officer.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      align="center"
+                      color="text.secondary"
+                    >
+                      {officer.position}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+
+        {/* Speech Committee Leaders */}
+        <Box sx={{ textAlign: "center", marginBottom: "2rem" }}>
+          <Typography
+            variant="h6"
+            sx={{ color: "white", fontWeight: "bold", marginBottom: "1rem" }}
+          >
+            Speech Committee Leaders
+          </Typography>
+
+          {/* Officers Grid */}
+          <Grid container spacing={4} justifyContent="center">
+            {speechCommitteeLeaders.map((officer) => (
+              <Grid item key={officer.name} xs={12} sm={6} md={4} lg={2}>
+                <Card
+                  sx={{
+                    backgroundColor: "#fff",
+                    borderRadius: "8px",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    image={officer.img}
+                    alt={officer.name}
+                    sx={{
+                      height: "200px",
+                      objectFit: "cover",
+                    }}
+                  />
+                  <CardContent>
+                    <Typography
+                      variant="subtitle1"
+                      align="center"
+                      sx={{ fontWeight: "bold" }}
+                    >
+                      {officer.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      align="center"
+                      color="text.secondary"
+                    >
+                      {officer.position}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+
+        {/* Debate Committee Leaders */}
+        <Box sx={{ textAlign: "center", marginBottom: "2rem" }}>
+          <Typography
+            variant="h6"
+            sx={{ color: "white", fontWeight: "bold", marginBottom: "1rem" }}
+          >
+            Debate Committee Leaders
+          </Typography>
+
+          {/* Officers Grid */}
+          <Grid container spacing={4} justifyContent="center">
+            {debateCommitteeLeaders.map((officer) => (
+              <Grid item key={officer.name} xs={12} sm={6} md={4} lg={2}>
+                <Card
+                  sx={{
+                    backgroundColor: "#fff",
+                    borderRadius: "8px",
+                    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                  }}
+                >
+                  <CardMedia
+                    component="img"
+                    image={officer.img}
+                    alt={officer.name}
+                    sx={{
+                      height: "200px",
+                      objectFit: "cover",
+                    }}
+                  />
+                  <CardContent>
+                    <Typography
+                      variant="subtitle1"
+                      align="center"
+                      sx={{ fontWeight: "bold" }}
+                    >
+                      {officer.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      align="center"
+                      color="text.secondary"
+                    >
+                      {officer.position}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
       </Box>
     </>
   );
